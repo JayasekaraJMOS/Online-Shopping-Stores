a3ef86843765e7bb30474ddaf11fff2127ae66f9
JayasekaraJMOS <omindusanvida1234.com>
2026-03-01 19:08:32 +0530

    feat: add types folder and Product interface

---
+++ src/types/Product.ts
@@ -0,0 +1,14 @@
+// This interface ensures we never use the 'any' type
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
